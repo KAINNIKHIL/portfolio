8bdc8dad9796f64b1162c772dc70d95429a3f171
Nikhil <nikhilsinghkaintura@gmail.com>
2025-08-25 16:59:03 +0530

    Initial portfolio commit

---
+++ portfolio.js
@@ -0,0 +1,36 @@
+const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-links");
+
+    menuToggle.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+    });
+
+
+    
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("nav a");
+
+    window.addEventListener("scroll", () => {
+      let current = "";
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 70;
+        if (pageYOffset >= sectionTop) {
+          current = section.getAttribute("id");
+        }
+      });
+      navLinks.forEach((a) => {
+        a.classList.remove("active");
+        if (a.getAttribute("href").includes(current)) {
+          a.classList.add("active");
+        }
+      });
+    });
+
+   
+    lottie.loadAnimation({
+      container: document.getElementById('dev-animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'Developer.json'
+    });
